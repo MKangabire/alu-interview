2eb2a8964ae5a75f4a3b6b0aa51b37d0ecdf9041
MKangabire <mkangabire@alustudent.com>
2024-01-29 09:00:29 +0000

    rain

---
+++ rain/0-rain.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def calculate_water(arr, n):
+    """calculates the maximum size of the walls"""
+	
+    max_size = 0
+    left_max = [0] * n
+    right_max = [0] * n
+
+    left_max[0] = arr[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i - 1], arr[i])
+
+    right_max[n - 1] = arr[n - 1]
+    for i in range(n - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], arr[i])
+
+    for i in range(1, n - 1):
+        max_size += max(0, min(left_max[i], right_max[i]) - arr[i])
+
+    return max_size
+
+def rain(walls):
+    """calculates the rain retained acording to the len of walls"""
+    water_consumed = calculate_water(walls, len(walls))
+    return water_consumed
